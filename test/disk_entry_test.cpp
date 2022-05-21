92539f8a148d31f5464bf7248e8982eb2908d2b8
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2022-05-21 21:03:55 +0530

    added disk entry file

---
+++ test/disk_entry_test.cpp
@@ -0,0 +1,40 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 Rishabh Dwivedi<rishabhdwivedi17@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "disk_entry.hpp"
+
+#include "concepts.hpp"
+#include "test_include.hpp"
+
+TEST_CASE("file concepts test") {
+  static_assert(untree::file<untree::disk_file>);
+}
+
+TEST_CASE("directory concepts test") {
+  static_assert(untree::directory<untree::disk_directory>);
+}
+
+TEST_CASE("entry concepts test") {
+  static_assert(untree::entry<untree::disk_entry>);
+}
